c7df3a3c6ece12f12bda01e9fd26a74681c749ef
Priyanshu Tyagi <154249208+Jakon777@users.noreply.github.com>
2026-03-13 22:14:27 +0530

    Update resume_routes.py

--- routes/resume_routes.py
+++ routes/resume_routes.py
@@ -1,18 +1,39 @@
 from fastapi import APIRouter, UploadFile, File
 import shutil, os
 from core.resume_parser import parse_resume
-from core.embedding_engine import generate_embedding
 
-router=APIRouter(prefix="/resume")
+router = APIRouter(prefix="/resume")
 
-UPLOAD_FOLDER="uploads"
-os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @router.post("/parse")
-async def parse(file:UploadFile=File(...)):
-    path=os.path.join(UPLOAD_FOLDER,file.filename)
-    with open(path,"wb") as buffer:
-        shutil.copyfileobj(file.file,buffer)
-    data=parse_resume(path)
-    data["embedding"]=generate_embedding(data["raw_text"])
+async def parse(file: UploadFile = File(...)):
+    path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    data = parse_resume(path)
+
     return data
+
+
+# from fastapi import APIRouter, UploadFile, File
+# import shutil, os
+# from core.resume_parser import parse_resume
+# from core.embedding_engine import generate_embedding
+
+# router=APIRouter(prefix="/resume")
+
+# UPLOAD_FOLDER="uploads"
+# os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+
+# @router.post("/parse")
+# async def parse(file:UploadFile=File(...)):
+#     path=os.path.join(UPLOAD_FOLDER,file.filename)
+#     with open(path,"wb") as buffer:
+#         shutil.copyfileobj(file.file,buffer)
+#     data=parse_resume(path)
+#     data["embedding"]=generate_embedding(data["raw_text"])
+#     return data
